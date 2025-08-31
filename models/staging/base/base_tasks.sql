75b7a0f53326bd90d21cc512e262bab423d92315
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-08-31 14:46:25 +0000

    chore: explicit cast array for base models

--- models/staging/base/base_tasks.sql
+++ models/staging/base/base_tasks.sql
@@ -16,16 +16,16 @@ renamed as (
       CAST(isallday AS BOOLEAN) as is_allday,
       CAST(priority AS INT) as priority,
       CAST(status AS INT) as status,
-      SPLIT(REGEXP_REPLACE(tags, r'^\[|\]$', ''), ',') as tags,
+      CAST(SPLIT(REGEXP_REPLACE(tags, r'^\[|\]$', ''), ',') as ARRAY<STRING>) as tags,
       CAST(columnid AS STRING) as column_id,
       CAST(etag AS STRING) as etag,
       CAST(kind AS STRING) as kind,
       CAST(repeatflag AS STRING) as repeat_flag,
       parse_timestamp("%FT%H:%M:%E3S%z",startdate) as start_date,
       parse_timestamp("%FT%H:%M:%E3S%z",duedate) as due_date,
-      SPLIT(REGEXP_REPLACE(reminders, r"^\[|\]$|\'", ''), ',') as reminders,
+      CAST(SPLIT(REGEXP_REPLACE(reminders, r"^\[|\]$|\'", ''), ',') as ARRAY<STRING>) as reminders,
       parse_timestamp("%FT%H:%M:%E3S%z",completedtime) as completed_time,
-      SPLIT(REGEXP_REPLACE(childids, r'^\[|\]$', ''), ',') as childids,
+      CAST(SPLIT(REGEXP_REPLACE(childids, r'^\[|\]$', ''), ',') as ARRAY<STRING>) as childids,
       CAST(parentid AS STRING) as parent_id
       from source
 )
