2e07eb4a076159b1df05ac65ebf7dffb87e32cab
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-09-07 16:53:44 +0000

    fix: snapshot stores stale data when stateless fetches new attributes

--- models/2_dump_snapshot_to_gcs/projects_snapshot.sql
+++ models/2_dump_snapshot_to_gcs/projects_snapshot.sql
@@ -4,4 +4,38 @@
     enabled=(target.name == 'dump_snapshot')
 ) }}
 
-select * from {{ref('snp_projects')}}
+with snp as (
+
+    select * from {{ref('snp_projects')}}
+
+),
+
+base as (
+    -- pulls in the stateless raw to inject 
+    -- the latest attributes before dumping 
+    -- to snapshot remote.
+    select * from {{source('stateless_raw','projects')}}
+),
+
+joined as (
+    select 
+    -- get base first if avail, else coalesce to snp
+    {%- set columns = adapter.get_columns_in_relation(ref('snp_projects')) -%}
+
+    {% for column in columns %}
+        {% if 'dbt_' not in column.name %}
+        COALESCE(b.{{column.name}}::text, s.{{column.name}}::text) as {{column.name}},
+        {% endif %}
+    {% endfor %}
+    dbt_scd_id,
+    dbt_updated_at,
+    dbt_valid_from,
+    dbt_valid_to
+
+    from snp s LEFT JOIN base b 
+    ON s.id = b.id
+
+
+)
+
+select * from joined
--- models/2_dump_snapshot_to_gcs/tasks_snapshot.sql
+++ models/2_dump_snapshot_to_gcs/tasks_snapshot.sql
@@ -4,7 +4,41 @@
     enabled=(target.name == 'dump_snapshot')
 ) }}
 
-select * from {{ref('snp_tasks')}}
+with snp as (
+    select * from {{ref('snp_tasks')}}
+),
+
+base as (
+    -- pulls in the stateless raw to inject 
+    -- the latest attributes before dumping 
+    -- to snapshot remote.
+    select * from {{source('stateless_raw','tasks')}}
+),
+
+joined as (
+    select 
+    -- get base first if avail, else coalesce to snp
+    {%- set columns = adapter.get_columns_in_relation(ref('snp_tasks')) -%}
+
+    {% for column in columns %}
+        {% if 'dbt_' not in column.name %}
+        COALESCE(b.{{column.name}}::text, s.{{column.name}}::text) as {{column.name}},
+        {% endif %}
+    {% endfor %}
+    dbt_scd_id,
+    dbt_updated_at,
+    dbt_valid_from,
+    dbt_valid_to
+
+    from snp s LEFT JOIN base b 
+    ON s.id = b.id
+
+
+)
+
+select * from joined
+
+
 -- needs to explicitly call out repeat tasks cause
 -- they get the same id when done (!)
 where repeatFlag is null
