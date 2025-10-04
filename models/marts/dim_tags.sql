72808ee25e7e8906c392288af914422e69b04853
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-10-04 06:51:40 +0000

    chore: address PR review

--- models/marts/dim_tags.sql
+++ models/marts/dim_tags.sql
@@ -1,5 +1,3 @@
-{# models/marts/dim_tags.sql #}
-
 {#
 This model creates a dimension table for tags. The grain of this table
 is one row per unique tag. A surrogate key (tag_id) is generated to
@@ -11,28 +9,15 @@ WITH
 task_tags AS (
     {# Source: Bridge model that unnests tags from tasks. #}
     SELECT
-        tag
+        distinct tag
     FROM
         {{ ref('stg__ticktick__task_tags') }}
-),
-
-distinct_tags AS (
-    {# De-duplicate the tags to get a unique list. #}
-    SELECT DISTINCT
-        tag
-    FROM
-        task_tags
 )
 
 SELECT
-    {#
-    Generate a surrogate key for the tag. This is a best practice for
-    dimension tables as it provides a stable key that won't change if the
-    tag name is ever modified.
-    #}
     {{ dbt_utils.generate_surrogate_key(['tag']) }} AS tag_id,
     tag
 FROM
-    distinct_tags
+    task_tags
 ORDER BY
     tag
