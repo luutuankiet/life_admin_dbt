4ab0829c15ab7f77ca8fbff9421dcf0a36a2e0e2
luutuankiet <56199834+luutuankiet@users.noreply.github.com>
2025-10-12 18:06:03 +0700

    Feat: Centralize Lookahead Analytics on `fct_tasks` Explore (#21)

---
+++ models/marts/dim_date_spine.sql
@@ -0,0 +1,20 @@
+{# models/marts/dim_date_spine.sql #}
+
+{#
+This model generates a continuous series of dates, providing a foundational
+timeline for forward-looking analysis. It can be joined to fact tables
+to ensure that all days are represented in a report, even if no events
+occurred on a given day.
+#}
+
+{%- set timezone = var('timezone') -%}
+{{ config(
+    materialized = 'table'
+)
+}}
+
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="CAST(" ~ dbt_date.today(timezone) ~ " AS DATE)",
+    end_date="CAST(" ~ dbt.dateadd('month', 3, dbt_date.today(timezone)) ~ " AS DATE)"
+) }}
--- models/staging/ticktick/stg__ticktick__task_tags.sql
+++ models/staging/ticktick/stg__ticktick__task_tags.sql
@@ -19,9 +19,27 @@ unnested as (
     from
         renamed,
         unnest(tags) as tag
+),
+
+task_without_tags as (
+    -- unnest above dont really work if 
+    -- the tags array len = 0 i.e.
+    -- the task do not have any tags.
+    select 
+        task_id,
+        'default' as tag
+    from renamed
+    where ARRAY_LENGTH(tags) = 0
+),
+
+unioned as (
+    select * from unnested
+    UNION ALL
+    select * from task_without_tags
+
 )
 
 SELECT
     *
 FROM
-    unnested
+    unioned
